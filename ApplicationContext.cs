89ae3b45f786b94f82e7341b318554ac71a31ea6
user <user@users-MacBook-Air.local>
2020-04-21 12:49:28 +0100

    initial commit

---
+++ ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCore.Models;
+
+namespace NetCore
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions opts) : base(opts)
+        {
+        }
+
+        public DbSet<Employee> employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NetCore.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string EmployeeId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+        
+    }
+}
